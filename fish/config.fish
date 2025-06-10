653e45c403bc81f1cf7085741c73ffbe1761f7f5
Kel Kanhirun <kel@native.dev>
2025-06-10 11:18:47 -0400

    Adds nvim as default editor for fish shell

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,10 @@
 # Unset the default fish greeting text which messes up Zellij
 set fish_greeting
 
+# Uses nvim as default editor
+set -x EDITOR nvim
+set -x VISUAL nvim
+
 # Add local bin to PATH
 fish_add_path $HOME/.local/bin
 
